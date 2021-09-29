6a649773efe47c83603d5f7c88e894bd59c7861b
Aleksander <aleksander.kononenko2016@yandex.ru>
2021-09-29 14:57:56 +0300

    fixed forms

---
+++ cookie/js/index.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('btn').addEventListener('click', () => {
+    document.getElementById('cookie').style.display = 'none';
+  });
+});
---
+++ subscription/js/index.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('close').addEventListener('click', () => {
+    document.getElementById('form').style.display = 'none';
+  });
+});
