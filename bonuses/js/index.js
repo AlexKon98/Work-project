330c73d87f0d79b2dfe487aa3eba7831b4670746
Aleksander <aleksander.kononenko2016@yandex.ru>
2021-10-10 18:12:50 +0300

    fixed faq

--- bonuses/js/index.js
+++ bonuses/js/index.js
@@ -1,22 +1,40 @@
 let tabs = document.querySelectorAll('.your-bonuses__link');
 let swipeContent = document.querySelectorAll('.swipe-content-active');
 
-document.querySelectorAll('.my-bonuses__link').forEach(function(tabs) {
-  tabs.addEventListener('click', function(click) {
-    const btn = click.currentTarget.dataset.path;
-    document.querySelectorAll('.my-bonuses__link').forEach(function(tab) {
-      tab.classList.remove('my-bonuses__link_active');
+
+tabs.forEach((tab) => {
+    tab.addEventListener('click', (event) => {
+        const path = event.currentTarget.dataset.path;
+        swipeContent.forEach((tabContent) => {
+            tabContent.classList.remove('your-bonuses__main-list');
+        });
+        document.querySelector(`[data-target="${path}"]`).classList.add('your-bonuses__main-list');
     });
-    document.querySelector(`[data-path="${btn}"]`).classList.add('my-bonuses__link_active');
+});
+
+tabs.forEach((active) => {
+  active.addEventListener('click', () => {
+    tabs.forEach((item) => {
+      item.classList.remove('your-bonuses__link_active')
+    });
+    active.classList.add('your-bonuses__link_active');
   });
 });
 
-tabs.forEach((tab) => {
-  tab.addEventListener('click', (event) => {
-    const path = event.currentTarget.dataset.path;
-    swipeContent.forEach((tabContent) => {
-      tabContent.classList.remove('your-bonuses__main-list');
-    });
-    document.querySelector(`[data-target="${path}"]`).classList.add('your-bonuses__main-list');
-  });
-});
+let bonuses_btns = document.querySelectorAll('.my-bonuses__link')
+let bonuses_li = document.querySelectorAll('.my-bonuses__item')
+let countHeart = document.querySelector('.my_bonuses_count')
+let bonusesTitle = document.querySelector('.my-bonuses__heading')
+
+
+
+bonuses_li.forEach(li => {
+    li.addEventListener('click', () => {
+        bonuses_btns.forEach(btn_rem => { btn_rem.classList.remove('my-bonuses__link_active') })
+        countHeart.classList.remove('my_bonuses_count_active')
+        li.querySelector('.my-bonuses__link').classList.add('my-bonuses__link_active')
+        if (li.querySelector('.my_bonuses_count')) {
+            countHeart.classList.add('my_bonuses_count_active')
+        }
+    })
+})
---
+++ faq/js/index.js
@@ -0,0 +1,15 @@
+let accordionItem = document.querySelectorAll('.faq__accordion__item');
+
+accordionItem.forEach((item) => {
+  item.addEventListener('click', () => {
+    let trigger = item.classList.contains('faq__accordion__item-active');
+    accordionItem.forEach((object) => {
+      object.classList.remove('faq__accordion__item-active');
+      object.querySelector('.accordion__heading').classList.remove('accordion__heading-active');
+    });
+    if (!trigger) {
+      item.classList.add('faq__accordion__item-active');
+      item.querySelector('.accordion__heading').classList.add('accordion__heading-active');
+    }
+  });
+});
