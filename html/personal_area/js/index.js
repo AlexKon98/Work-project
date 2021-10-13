f09c96936e65b52c4c297ade23e17ef74650e4a2
Aleksander <aleksander.kononenko2016@yandex.ru>
2021-10-13 16:43:12 +0300

    changed personal area page

--- html/personal_area/js/index.js
+++ html/personal_area/js/index.js
@@ -8,3 +8,19 @@ link.forEach((li) => {
     li.classList.add('personal_area__link_active');
   });
 });
+
+const form = document.getElementById('form');
+const btn = document.getElementById('submit');
+const ref = document.getElementById('ref');
+
+form.addEventListener('submit', (f) => {
+  f.preventDefault();
+  document.getElementById('name').setAttribute('disabled', 'disabled');
+  document.getElementById('tel').setAttribute('disabled', 'disabled');
+}, true);
+
+btn.addEventListener('click', () => {
+  if(ref.value) {
+    ref.setAttribute('disabled', 'disabled');
+  }
+})
