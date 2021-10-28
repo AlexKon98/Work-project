d5614cad9edcfafa729e66c7e281aa63d3cc393d
Aleksander <aleksander.kononenko2016@yandex.ru>
2021-10-28 19:16:22 +0300

    adaptive

--- html/Вопросы и ответы/js/index.js
+++ html/Вопросы и ответы/js/index.js
@@ -1,4 +1,5 @@
 let accordionItem = document.querySelectorAll('.faq__accordion__item');
+let menuBtn = document.querySelector('.menu__btn');
 
 accordionItem.forEach((item) => {
   item.addEventListener('click', () => {
@@ -13,3 +14,20 @@ accordionItem.forEach((item) => {
     }
   });
 });
+
+
+menuBtn.addEventListener('click', () => {
+  menuBtn.classList.toggle('open-menu');
+  if (menuBtn.classList.contains('open-menu')) {
+    menuBtn.innerHTML = `<svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M24.853 1.14697L1.00002 25" stroke="#276F97" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M1.14746 1L25.0005 24.853" stroke="#276F97" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>`;
+  } else {
+    menuBtn.innerHTML = `<svg width="32" height="24" viewbox="0 0 32 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M1 11.9092H31" stroke="#276F97" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M1 1H31" stroke="#276F97" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M1 22.8184H31" stroke="#276F97" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>`;
+  }
+})
